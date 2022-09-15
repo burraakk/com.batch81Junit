0a8547c9ad2b28853a65794a0f051094883f1fc3
Burak Öz <burakoz2006@gmail.com>
2022-09-16 01:53:18 +0300

    sep15

---
+++ src/test/java/day06/C02_Assertion.java
@@ -0,0 +1,75 @@
+package day06;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C02_Assertion {
+
+    // Not : BeforeAfter methodunda testleri farklı sayfalarda açıp kapatıyor,
+    // BeforeAfterCLass methodlarında aynı sayfada açıp sonra kapatıyor.
+    // BeforeAfter methodunda methodların static yapmaya gerek yok ama
+    // BeforeAfterCLass'da static yapmak zorunlu.
+
+    //assertTrue
+    //beklenen bir sonucun true olduğunun kabul edilmesi gerektiği zaman kullanılır.
+    // Parametre olarak iki değer alır.
+    // İlk parametre de bir mesaj gönderilir ikinci parametrede ise gönderilen mesajın doğruluğu için koşul belirlenir.
+    //assertFalse
+    //beklenen bir sonucun false olması durumunda kullanılır.
+    // İki parametre alır. Parametrelerden biri mesajdır diğeri ise koşuldur.
+    // assertFalse ile koşul yerine getirilmez ise assertionError hatası fırlatır.
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://amazon.com");
+
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void test1(){
+        //- url'in "amazon" icergini test et
+        String expectedURL = "amazon";
+        String actualURL = driver.getCurrentUrl();
+        Assert.assertTrue(actualURL.contains(expectedURL));
+    }
+
+    @Test
+    public void test2(){
+        //- title'in "facebook" icermedigini test et
+        String expectedTitle = "facebook";
+        String actualTitle = driver.getTitle();
+        Assert.assertFalse(actualTitle.contains(expectedTitle));
+
+    }
+
+    @Test
+    public void test3(){
+        //- sol ust kosede amazon logosunun gorundugunu test et
+        WebElement logo = driver.findElement(By.id("nav-logo-sprites"));
+        Assert.assertTrue(logo.isDisplayed());
+
+    }
+
+
+
+}
--- src/test/java/tasks/automationExercise/Exercise01.java
+++ src/test/java/tasks/automationExercise/Exercise01.java
@@ -5,6 +5,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -29,7 +30,7 @@ public class Exercise01 {
     }
 
     @Test
-    public void test1(){
+    public void test1() throws InterruptedException {
 
         //1. Launch browser
         //2. Navigate to url 'http://automationexercise.com'
@@ -55,9 +56,44 @@ public class Exercise01 {
             System.out.println("New User Signup Button is not visible. Test FAILED");
 
         //6. Enter name and email address
+        WebElement nameBox = driver.findElement(By.xpath("//*[@data-qa='signup-name']"));
+        WebElement emailBox = driver.findElement(By.xpath("//*[@data-qa='signup-email']"));
+        nameBox.sendKeys("Burak");
+        emailBox.sendKeys("enesyildizz90@gmail.com");
+
         //7. Click 'Signup' button
+        WebElement signUpButton = driver.findElement(By.xpath("//*[@data-qa='signup-button']"));
+        signUpButton.click();
+
         //8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
+        WebElement enterAccountInfo = driver.findElement(By.xpath("//*[text()='Enter Account Information']"));
+        if (enterAccountInfo.isDisplayed()){
+            System.out.println("ENTER ACCOUNT INFORMATION is visible. Test PASSED");
+        }else
+            System.out.println("ENTER ACCOUNT INFORMATION is not visible. Test FAILED");
+
         //9. Fill details: Title, Name, Email, Password, Date of birth
+        WebElement titleMr = driver.findElement(By.xpath("//*[@value='Mr']"));
+        WebElement nameBoxSignUpPage = driver.findElement(By.xpath("//*[@data-qa='name']"));
+        WebElement emailBoxSignUpPage = driver.findElement(By.xpath("//*[@data-qa='email']"));
+        WebElement passwordSignUpPage = driver.findElement(By.xpath("//*[@data-qa='password']"));
+        WebElement dayBox = driver.findElement(By.xpath("//*[@id='days']"));
+        WebElement monthBox = driver.findElement(By.xpath("//*[@id='months']"));
+        WebElement yearBox = driver.findElement(By.xpath("//*[@id='years']"));
+
+        Thread.sleep(1000);
+        titleMr.click();
+        Thread.sleep(1000);
+        passwordSignUpPage.sendKeys("12345");
+        Thread.sleep(1000);
+        dayBox.click();
+        dayBox.sendKeys("17");
+        Thread.sleep(1000);
+        monthBox.click();
+        monthBox.sendKeys("April");
+        Thread.sleep(1000);
+        yearBox.sendKeys("1995");
+
         //10. Select checkbox 'Sign up for our newsletter!'
         //11. Select checkbox 'Receive special offers from our partners!'
         //12. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number

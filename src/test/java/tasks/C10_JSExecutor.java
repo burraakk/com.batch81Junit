334eae59a5c0128d9cbc3446ffe5ab3613d75492
Burak Öz <burakoz2006@gmail.com>
2022-09-28 17:12:01 +0300

    sep28

--- src/test/java/tasks/C10_JSExecutor.java
+++ src/test/java/tasks/C10_JSExecutor.java
@@ -1,15 +1,56 @@
 package tasks;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import utilities.TestBaseBeforeClassAfterClass;
 
 public class C10_JSExecutor extends TestBaseBeforeClassAfterClass {
-    @Test
-    public void test() {
-        //hotelmycamp anasayfasina gidin
 
-        //2 farkli test method’u olusturarak actions classi ve Js Executor kullanarak
+    //hotelmycamp anasayfasina gidin
+    //2 farkli test method’u olusturarak actions classi ve Js Executor kullanarak
+    //asagidaki oda turlerinden  ust sira ortadaki odayi tiklayin
+    //istediginiz oda inceleme sayfasi acildigini test  edin
+
+
+    @Test
+    public void testActions() throws InterruptedException {
+        //hotelmycamp anasayfasina gidin
+        driver.get("https://www.hotelmycamp.com/");
         //asagidaki oda turlerinden  ust sira ortadaki odayi tiklayin
+
+        actions.sendKeys(Keys.PAGE_DOWN).
+                sendKeys(Keys.PAGE_DOWN).
+                sendKeys(Keys.PAGE_DOWN).
+                perform();
+        WebElement room = driver.findElement(By.xpath("//p/a[@href='/RoomDetail/425']"));
+        Thread.sleep(2000);
+        actions.click(room).perform();
+
         //istediginiz oda inceleme sayfasi acildigini test  edin
+        WebElement roomHeader = driver.findElement(By.xpath("//h1[text()='dsafsaf']"));
+        Assert.assertTrue(roomHeader.isDisplayed());
+
+    }
+
+    @Test
+    public void testJSExecutor() throws InterruptedException {
+        //hotelmycamp anasayfasina gidin
+        driver.get("https://www.hotelmycamp.com/");
+
+        //asagidaki oda turlerinden  ust sira ortadaki odayi tiklayin
+        WebElement room = driver.findElement(By.xpath("//p/a[@href='/RoomDetail/425']"));
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("arguments[0].scrollIntoView(true);",room);
+        Thread.sleep(2000);
+        jse.executeScript("arguments[0].click();",room);
+
+        //istediginiz oda inceleme sayfasi acildigini test  edin
+        WebElement roomHeader = driver.findElement(By.xpath("//h1[text()='dsafsaf']"));
+        Assert.assertTrue(roomHeader.isDisplayed());
+
     }
 }
